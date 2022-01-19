a4e75ff1c178682e5aebd3386faf54091b24dfbc
RAYNARD DODZI HELEGAH <dodzireynard@gmail.com>
2022-01-19 04:28:11 +0000

    Revert "fix: update readme instructions on docker"
    This reverts commit 00e917e52b75b8f75c92c1ee5323bbd3264c8a14.

--- solution/app/main.py
+++ solution/app/main.py
@@ -19,14 +19,12 @@ def get_db():
 
 @app.get("/items/{item_id}", response_model=schemas.Item)
 def get_item(item_id: int, db: Session = Depends(get_db)):
-    """Returns an item with the specified id or None if it does not exist."""
     item = crud.get_item(db, item_id=item_id)
     return item
 
 
 @app.post("/items/", response_model=schemas.Item)
 def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    """Creates and returns an item in the database using the posted data.."""
     return crud.create_item(db=db, item=item)
 
 
@@ -34,8 +32,7 @@ def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
 def update_item(item: schemas.ItemUpdate,
                 item_id: int,
                 db: Session = Depends(get_db)):
-    """Updates and retuns an item with the specified id or returns None if it does not exist."""
     db_item = crud.get_item(db, item_id=item_id)
     if not db_item:
-        return db_item
+        return HTTPException(404, detail="Item not found.")
     return crud.update_item(db=db, item_id=item_id, item=item)
